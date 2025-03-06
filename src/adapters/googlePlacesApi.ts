e4516ac89b06bb59bc3cd03aa57240e09c721d03
moonfloof <git@moonfloof.com>
2025-03-06 20:30:05 +0000

    checkins: Cleanup edge cases

--- src/adapters/googlePlacesApi.ts
+++ src/adapters/googlePlacesApi.ts
@@ -1,6 +1,6 @@
 import phin from 'phin';
-import { config } from '../lib/config.js';
 import { type CheckinPlace, getCachedPlace } from '../database/checkins.js';
+import { config } from '../lib/config.js';
 import Logger from '../lib/logger.js';
 
 const log = new Logger('google-places');
@@ -25,6 +25,30 @@ interface NearbyPlace {
 interface NearbyPlacesResponse {
 	places: NearbyPlace[];
 }
+
+/*
+
+# Searching process documentation - to be implemented if more than 5000 requests
+# are made in one month (very unlikely).
+
+Because the Google Places API is billed, we want to make as few requests to it
+as reasonably possible and HEAVILY cache instead. This relies on two database
+tables. One for cached places, and another for searched coordinates and their
+radii.
+
+When we call the API, we're given a total of 20 places within the radius of our
+location. Without knowing how many total places are actually within 500 metres,
+we should make note of the distance the furthest place away is. When caching
+these results, record all places in the database as-is, and separately record
+our current location with this maximum radius. When we make subsequent requests
+to the `searchNearbyPlaces` function, if our radius has any area not already
+covered by previous searches: perform another search using the API, merge or
+ignore any previously caches places, and add our maximum search radius as
+another entry. Therefore, we should eventually find that calling
+`searchNearbyPlaces` doesn't need to call the Google API, thus saving request
+credits.
+
+*/
 
 export async function searchNearbyPlaces(lat: number, long: number): Promise<CheckinPlace[]> {
 	const { placesApiEnabled, apiKey } = config.google;
