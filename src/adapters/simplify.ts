13a342dedb37e0b5acc645c33d6a47c24b0cbea9
moonfloof <git@moonfloof.com>
2025-03-15 00:06:12 +0000

    Add license to simplify.ts

--- src/adapters/simplify.ts
+++ src/adapters/simplify.ts
@@ -1,16 +1,38 @@
 /*
- (c) 2017, Vladimir Agafonkin
  Simplify.js, a high-performance JS polyline simplification library
  mourner.github.io/simplify-js
+
+ Copyright (c) 2017, Vladimir Agafonkin
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ -----
 
  Modification by moonfloof in 2025, to add types to functions and accept
  [number, number][] as an input.
 */
 
 type Point = [number, number];
-
-// to suit your point format, run search/replace for '.x' and '.y';
-// for 3D version, see 3d branch (configurability would draw significant performance overhead)
 
 // square distance between 2 points
 function getSqDist(p1: Point, p2: Point) {
@@ -44,7 +66,6 @@ function getSqSegDist(p: Point, p1: Point, p2: Point) {
 
 	return dx * dx + dy * dy;
 }
-// rest of the code doesn't care about point format
 
 // basic distance-based simplification
 function simplifyRadialDist(points: Point[], sqTolerance: number) {
